2448430e0d78189f8f04f648a1eea37642f999f2
Lauri Kurki <lauri.kurki@siili.com>
2020-10-31 14:23:38 +0200

    Add prettier config, remove svelte.d.ts (not required)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,10 @@
-import App from "./App.svelte";
+import App from './App.svelte'
 
 const app = new App({
-  target: document.body,
-  props: {
-    name: "hello from the otherside",
-  },
-});
+    target: document.body,
+    props: {
+        name: 'hello from the otherside',
+    },
+})
 
-export default app;
+export default app
--- frontend/src/svelte.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "*.svelte" {
-  class Element {
-    constructor(args: { target: HTMLElement; props: { [key: string]: any } });
-  }
-
-  export default Element;
-}
